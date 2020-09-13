447d8fe820f875516a8f999cce0537a1abadd981
yoyojacky <yoyojacky2009@gmail.com>
2020-09-13 23:05:34 +0800

    update install.sh script
    Signed-off-by: yoyojacky <yoyojacky2009@gmail.com>

--- install.sh
+++ install.sh
@@ -1,60 +1,65 @@
 #!/bin/bash
 # 
-daemonname="deskpid"
+daemonname="deskpi"
 tempmonscript=/usr/bin/pmwFanControl
-daemonfanservice=/lib/systemd/system/$daemonname.service
-fanshutdownservice=/lib/systemd/system-shutdown/$daemonname-shutdown.service
+deskpidaemon=/lib/systemd/system/$daemonname.service
+stopfandaemon=/lib/systemd/system-shutdown/$daemonname-shutdown.service
 installationfolder=/home/pi/deskpi
 
 # Create service file on system.
-sudo touch $fanshutdownservice 
-sudo chmod 666 $fanshutdownservice
+if [ -e $deskpidaemon ]; then
+	sudo rm $deskpidaemon
+fi
+
+sudo touch $stopfandaemon 
+sudo chmod 666 $stopfandaemon
 
 # install PWM fan control daemon.
 cd $installationfolder/drivers/c/ && make
 sudo cp -rf $installationfolder/drivers/c/pwmFanControl /usr/bin/pwmFanControl
 sudo cp -rf $installationfolder/drivers/c/fanStop  /usr/bin/fanStop
-sudo chmod 666 /usr/bin/pwmFanControl
-sudo chmod 666 /usr/bin/fanStop
+sudo chmod 755 /usr/bin/pwmFanControl
+sudo chmod 755 /usr/bin/fanStop
 
 # Build Fan Daemon
-echo "[Unit]" >> $daemonfanservice
-echo "Description=DeskPi Fan Service" >> $daemonfanservice
-echo "After=multi-user.target" >> $daemonfanservice
-echo '[Service]' >> $daemonfanservice
-echo 'Type=simple' >> $daemonfanservice
-echo "Restart=always" >> $daemonfanservice
-echo "RemainAfterExit=true" >> $daemonfanservice
-echo "ExecStart=/usr/bin/pwmFanControl &" >> $daemonfanservice
-echo '[Install]' >> $daemonfanservice
-echo "WantedBy=multi-user.target" >> $daemonfanservice
+echo "[Unit]" >> $deskpidaemon
+echo "Description=DeskPi Fan Service" >> $deskpidaemon
+echo "After=multi-user.target" >> $deskpidaemon
+echo "" >> $daemonfanservic 
+echo "[Service]" >> $deskpidaemon
+echo "Type=simple" >> $deskpidaemon
+echo "Restart=always" >> $deskpidaemon
+echo "RemainAfterExit=true" >> $deskpidaemon
+echo "ExecStart=/usr/bin/pwmFanControl &" >> $deskpidaemon
+echo "[Install]" >> $deskpidaemon
+echo "WantedBy=multi-user.target" >> $deskpidaemon
 
 # Make it works
-sudo chmod 644 $daemonfanservice
+sudo chmod 644 $deskpidaemon
 sudo systemctl enable $daemonname.service
 sudo systemctl start $daemonname.service
 
 # send signal to MCU before system shuting down.
 
-echo "[Unit]" >> $fanshutdownservice
-echo "Description=Send shutdown signal to MCU at shutdown only" >> $fanshutdownservice
-echo "DefaultDependencies=no" >> $fanshutdownservice
-echo "Conflicts=reboot.target" >> $fanshutdownservice
-echo "Before=poweroff.target halt.target shutdown.target" >> $fanshutdownservice
-echo "Requires=poweroff.target" >> $fanshutdownservice
-echo "" >> $fanshutdownservice
-echo "[Service]" >> $fanshutdownservice
-echo "Type=oneshot" $fanshutdownservice
-echo "ExecStart=/usr/bin/fanStop &" >> $fanshutdownservice
-echo "RemainAfterExit=yes" >> $fanshutdownservice
-echo "" >> $fanshutdownservice
-echo "[Install]" >> $fanshutdownservice
-echo "WantedBy=shutdown.target" >> $fanshutdownservice
+echo "[Unit]" >> $stopfandaemon
+echo "Description=Send shutdown signal to MCU at shutdown only" >> $stopfandaemon
+echo "DefaultDependencies=no" >> $stopfandaemon
+echo "Conflicts=reboot.target" >> $stopfandaemon
+echo "Before=poweroff.target halt.target shutdown.target" >> $stopfandaemon
+echo "Requires=poweroff.target" >> $stopfandaemon
+echo "" >> $stopfandaemon
+echo "[Service]" >> $stopfandaemon
+echo "Type=oneshot" $stopfandaemon
+echo "ExecStart=/usr/bin/fanStop &" >> $stopfandaemon
+echo "RemainAfterExit=yes" >> $stopfandaemon
+echo "" >> $stopfandaemon
+echo "[Install]" >> $stopfandaemon
+echo "WantedBy=shutdown.target" >> $stopfandaemon
 
 # Make it works 
-sudo chmod 644 $fanshutdownservice
-sudo systemctl enable $fanshutdownservice
-sudo systemctl start  $fanshutdownservice
+sudo chmod 644 $stopfandaemon
+sudo systemctl enable $stopfandaemon
+sudo systemctl start  $stopfandaemon
 
 # Finished 
 echo -e "DeskPi Fan control script installation finished." 
