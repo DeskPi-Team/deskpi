a2cd55035fcca9f46b17c6f981766fbf07f2798a
yoyojacky <yoyojacky2009@gmail.com>
2020-09-15 12:26:56 +0800

    update install and uninstall script
    Signed-off-by: yoyojacky <yoyojacky2009@gmail.com>

--- uninstall.sh
+++ uninstall.sh
@@ -4,16 +4,21 @@
 
 daemonname="deskpi"
 deskpidaemon=/lib/systemd/system/$daemonname.service
-stopfandaemon=/lib/systemd/system-shutdown/fanStop.sh
+safeshutdaemon=/lib/systemd/system/$daemonname-safeshut.service
 
-log_action_msg "Uninstalling deskpi fan scipt..."
+log_action_msg "Uninstalling DeskPi PWM Fan Control and Safeshut Service."
 sleep 1
+log_action_msg "Diable DeskPi PWM Fan Control and Safeshut Service."
+log_action_msg "Remove dtoverlay configure from /boot/config.txt file"
+sudo sed -i '/dtoverlay=dwc2,dr_mode=host/d' /boot/config.txt
+log_action_msg "Stop and disable DeskPi services"
 sudo systemctl disable $daemonname.service 2&>/dev/null  
 sudo systemctl stop $daemonname.service  2&>/dev/null
-sudo systemctl disable $stopfandaemon.service 2&>/dev/null
-sudo systemctl stop $stopfandaemon.service 2&>/dev/null
-sudo rm -f  $deskpidaemon  2&>/dev/null || echo "remove $deskdaemon"
-sudo rm -f  $stopfandaemon 2&>/dev/null || echo "remove $stopfandaemon"
+sudo systemctl disable $daemonname-safeshut.service 2&>/dev/null
+sudo systemctl stop $daemonname-safeshut.service 2&>/dev/null
+log_action_msg "Remove DeskPi PWM Fan Control and Safeshut Service."
+sudo rm -f  $deskpidaemon  2&>/dev/null 
+sudo rm -f  $safeshutdaemon 2&>/dev/null 
 sudo rm -f /usr/bin/fanStop 2&>/dev/null
 sudo rm -f /usr/bin/pwmFanControl 2&>/dev/null 
-log_success_msg "Uninstalling deskpi is finished, have a nice day" 
+log_success_msg "Uninstall DeskPi Driver Successfully." 
