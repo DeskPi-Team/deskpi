64e11fd4aeeedd6616f12b4e3727a9eb1e38494d
boschkundendienst <boschkundendienst@googlemail.com>
2021-08-17 13:10:57 +0200

    removed sudo added safeshut

--- uninstall-manjaro.sh
+++ uninstall-manjaro.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 # uninstall deskpi  script 
+#
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root or with sudo"
+  exit
+fi
 
-deskpi=/lib/systemd/system/systemd-deskpi-safecutoffpower.service
+deskpi=/lib/systemd/system/deskpi.service
+safeshutdaemon=/lib/systemd/system/deskpi-safeshut.service
 echo "DeskPi Driver Uninstalling..."
 echo "Configure /boot/config.txt"
-sudo sed -i '/dtoverlay=dwc2,dr_mode=host/d' /boot/config.txt
+sed -i '/dtoverlay=dwc2,dr_mode=host/d' /boot/config.txt
 echo "Stop and disable DeskPi  services"
-sudo rm -f $deskpi 2&>/dev/null 
-sudo rm -f /usr/bin/safecutoffpower* 2&>/dev/null
-sudo rm -rf /etc/modules-load.d/raspberry.conf 2&>/dev/null
+systemctl stop deskpi.service
+systemctl stop deskpi-safeshut.service
+rm -f "$deskpi" 2&>/dev/null
+rm -f "$safeshutdaemon" 2&>/dev/null
+rm -f /usr/bin/safecutoffpower* 2&>/dev/null
+rm -f /usr/bin/pwmControlFan* 2&>/dev/null
+rm -rf /etc/modules-load.d/raspberry.conf 2&>/dev/null
 echo "Uninstall DeskPi Driver Successfully." 
 
