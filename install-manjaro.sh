f395062ae5680097b28bff5d717b79ac8e06727a
boschkundendienst <boschkundendienst@googlemail.com>
2021-08-17 10:45:49 +0200

    removed sudo command because sudo might not be present also sudo can not be redirect with '>>' one might use 'tee -a' instead.

--- install-manjaro.sh
+++ install-manjaro.sh
@@ -2,7 +2,7 @@
 # 
 echo "DeskPi Driver Installing..."
 if [ -d /tmp/deskpi ]; then
-	sudo rm -rf /tmp/deskpi 2&>/dev/null
+	rm -rf /tmp/deskpi 2&>/dev/null
 fi
 echo "Download the latest DeskPi Driver from GitHub..."
 cd /tmp && git clone https://github.com/DeskPi-Team/deskpi.git 
@@ -13,48 +13,48 @@ driverfolder=/tmp/deskpi
 
 # delete deskpi-safecutoffpower.service file.
 if [ -e $deskpi ]; then
-	sudo sh -c "rm -f $deskpi"
+	sh -c "rm -f $deskpi"
 fi
 
 # adding dtoverlay to enable dwc2 on host mode.
 echo "Configure /boot/config.txt file and enable front USB2.0"
-sudo sed -i '/dtoverlay=dwc2*/d' /boot/config.txt
-sudo sed -i '$a\dtoverlay=dwc2,dr_mode=host' /boot/config.txt 
-sudo sh -c "echo dwc2 > /etc/modules-load.d/raspberry.conf" 
+sed -i '/dtoverlay=dwc2*/d' /boot/config.txt
+sed -i '$a\dtoverlay=dwc2,dr_mode=host' /boot/config.txt 
+sh -c "echo dwc2 > /etc/modules-load.d/raspberry.conf" 
 
-sudo cp -rf $driverfolder/drivers/c/safecutoffpower64 /usr/bin/safecutoffpower64
-sudo cp -rf $driverfolder/drivers/python/safecutoffpower.py /usr/bin/safecutoffpower.py
-sudo chmod 644 /usr/bin/safecutoffpower64
-sudo chmod 644 /usr/bin/safecutoffpower.py
+cp -rf $driverfolder/drivers/c/safecutoffpower64 /usr/bin/safecutoffpower64
+cp -rf $driverfolder/drivers/python/safecutoffpower.py /usr/bin/safecutoffpower.py
+chmod 644 /usr/bin/safecutoffpower64
+chmod 644 /usr/bin/safecutoffpower.py
 
 # send cut off power signal to MCU before system shuting down.
-sudo echo "[Unit]" > $deskpi
-sudo echo "Description=DeskPi Safe Cut-off Power Service" >> $deskpi
-sudo echo "Conflicts=reboot.target" >> $deskpi
-sudo echo "DefaultDependencies=no" >> $deskpi
-sudo echo "" >> $deskpi
-sudo echo "[Service]" >> $deskpi
-sudo echo "Type=oneshot" >> $deskpi
-sudo echo "ExecStart=/usr/bin/sudo /usr/bin/safecutoffpower64" >> $deskpi
-sudo echo "# ExecStart=/usr/bin/sudo python3 /usr/bin/safecutoffpower.py" >> $deskpi
-sudo echo "RemainAfterExit=yes" >> $deskpi
-sudo echo "TimeoutStartSec=15" >> $deskpi
-sudo echo "" >> $deskpi
-sudo echo "[Install]" >> $deskpi
-sudo echo "WantedBy=halt.target shutdown.target poweroff.target final.target" >> $deskpi
+echo "[Unit]" > $deskpi
+echo "Description=DeskPi Safe Cut-off Power Service" >> $deskpi
+echo "Conflicts=reboot.target" >> $deskpi
+echo "DefaultDependencies=no" >> $deskpi
+echo "" >> $deskpi
+echo "[Service]" >> $deskpi
+echo "Type=oneshot" >> $deskpi
+echo "ExecStart=/usr/bin//usr/bin/safecutoffpower64" >> $deskpi
+echo "# ExecStart=/usr/bin/python3 /usr/bin/safecutoffpower.py" >> $deskpi
+echo "RemainAfterExit=yes" >> $deskpi
+echo "TimeoutStartSec=15" >> $deskpi
+echo "" >> $deskpi
+echo "[Install]" >> $deskpi
+echo "WantedBy=halt.target shutdown.target poweroff.target final.target" >> $deskpi
 
-sudo chown root:root $deskpi
-sudo chmod 644 $deskpi
+chown root:root $deskpi
+chmod 644 $deskpi
 
-sudo systemctl daemon-reload
-sudo systemctl enable systemd-deskpiv1-safecutoffpower.service
+systemctl daemon-reload
+systemctl enable systemd-deskpiv1-safecutoffpower.service
 # install rpi.gpio for fan control
-yes |sudo pacman -S python-pip
-sudo pip3 install pyserial
-# sudo pacman -S python python-pip base-devel
+yes |pacman -S python-pip
+pip3 install pyserial
+# pacman -S python python-pip base-devel
 # env CFLAGS="-fcommon" pip install rpi.gpio
 
 sync
-sudo rm -rf /tmp/deskpi
+rm -rf /tmp/deskpi
 echo "DeskPi Driver installation successful, system will reboot in 5 seconds to take effect!"
-sleep 5 && sudo reboot
+sleep 5 && reboot
