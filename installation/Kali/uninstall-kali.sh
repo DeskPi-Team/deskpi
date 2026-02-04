c5df8b80532ad0b7b91eec6e0b4eeefd3b803bd3
yoyojacky <yoyojacky@users.noreply.github.com>
2026-02-04 15:29:34 +0800

    Update uninstall-kali.sh
    remove log_action_msg

--- installation/Kali/uninstall-kali.sh
+++ installation/Kali/uninstall-kali.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # uninstall deskpi script 
-. /lib/lsb/init-functions
+#. /lib/lsb/init-functions
 
 daemonname="deskpi"
 deskpidaemon=/lib/systemd/system/$daemonname.service
 safeshutdaemon=/lib/systemd/system/$daemonname-safeshut.service
 
-log_action_msg "Uninstalling DeskPi PWM Fan Control and Safeshut Service."
+echo  "Uninstalling DeskPi PWM Fan Control and Safeshut Service."
 sleep 1
-log_action_msg "Diable DeskPi PWM Fan Control and Safeshut Service."
-log_action_msg "Remove dtoverlay configure from /boot/config.txt file"
+echo  "Diable DeskPi PWM Fan Control and Safeshut Service."
+echo  "Remove dtoverlay configure from /boot/config.txt file"
 sudo sed -i '/dtoverlay=dwc2,dr_mode=host/d' /boot/firmware/config.txt
-log_action_msg "Stop and disable DeskPi services"
+echo  "Stop and disable DeskPi services"
 sudo systemctl disable $daemonname.service 2&>/dev/null  
 sudo systemctl stop $daemonname.service  2&>/dev/null
 sudo systemctl disable $daemonname-safeshut.service 2&>/dev/null
 sudo systemctl stop $daemonname-safeshut.service 2&>/dev/null
-log_action_msg "Remove DeskPi PWM Fan Control and Safeshut Service."
+echo  "Remove DeskPi PWM Fan Control and Safeshut Service."
 sudo rm -f  $deskpidaemon  2&>/dev/null 
 sudo rm -f  $safeshutdaemon 2&>/dev/null 
 sudo rm -f /usr/bin/fanStop 2&>/dev/null
@@ -24,4 +24,4 @@ sudo rm -f /usr/bin/pwmFanControl64 2&>/dev/null
 sudo rm -f /usr/bin/safeCutOffPower64 2&>/dev/null 
 sudo rm -f /usr/bin/deskpi-config 2&>/dev/null 
 sudo rm -f /usr/bin/Deskpi-uninstall 2&>/dev/null 
-log_success_msg "Uninstall DeskPi Driver Successfully." 
+echo "Uninstall DeskPi Driver Successfully." 
